7d63a5610c6dacdf8816da8d6c4cac57b47b7677
CarolinaCalvi <jcalvi.carolina@gmail.com>
2026-02-22 21:49:10 -0300

    feat: add dags

--- airflow/include/utils/datasets.py
+++ airflow/include/utils/datasets.py
@@ -5,8 +5,8 @@ def bronze_dataset(project_name:str, schema_key: str) -> Dataset:
     # O que importa é o URI ser consistente entre produtor/consumidor
     return Dataset(f"bq://{project_name}/bronze/{schema_key}")
 
-def prata_dataset(project_name:str, schema_key: str) -> Dataset:
-    return Dataset(f"bq://{project_name}/prata/{schema_key}")
+def silver_dataset(project_name:str, schema_key: str) -> Dataset:
+    return Dataset(f"bq://{project_name}/silver/{schema_key}")
 
-def ouro_dataset(project_name:str, schema_key: str) -> Dataset:
-    return Dataset(f"bq://{project_name}/ouro/{schema_key}")
+def gold_dataset(project_name:str, schema_key: str) -> Dataset:
+    return Dataset(f"bq://{project_name}/gold/{schema_key}")
