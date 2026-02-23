4c6e42ca2639fe3d32285e1bb688dab735b906f2
CarolinaCalvi <jcalvi.carolina@gmail.com>
2026-02-22 23:54:43 -0300

    .

--- tests/bronze/test_extract_list_breweries_strategy.py
+++ tests/bronze/test_extract_list_breweries_strategy.py
@@ -1,6 +1,5 @@
 import pytest
-from unittest.mock import patch, AsyncMock, MagicMock
-
+from aioresponses import aioresponses
 from strategys.extract_list_breweries_strategy import ExtractListBreweriesStrategy
 
 @pytest.mark.asyncio
@@ -9,27 +8,10 @@ async def test_extract_list_breweries_strategy():
     strategy = ExtractListBreweriesStrategy(total_breweries=100)
     strategy.base_url = "http://test_url"
     
-    # We expect 2 pages (50 per page)
-    mock_response_1 = AsyncMock()
-    mock_response_1.status = 200
-    mock_response_1.json = AsyncMock(return_value=[{"id": "1"}])
-    
-    mock_get_1 = AsyncMock()
-    mock_get_1.__aenter__.return_value = mock_response_1
-    
-    mock_response_2 = AsyncMock()
-    mock_response_2.status = 200
-    mock_response_2.json = AsyncMock(return_value=[{"id": "2"}])
-    
-    mock_get_2 = AsyncMock()
-    mock_get_2.__aenter__.return_value = mock_response_2
-
-    mock_session = AsyncMock()
-    # Return different pages sequentially
-    mock_session.get.side_effect = [mock_get_1, mock_get_2]
-
-    with patch("strategys.extract_list_breweries_strategy.ClientSession", return_value=mock_session) as MockClientSession:
-        MockClientSession.return_value.__aenter__.return_value = mock_session
+    with aioresponses() as m:
+        # We expect 2 pages (50 per page). 
+        m.get('http://test_url?page=0&per_page=50', payload=[{"id": "1"}])
+        m.get('http://test_url?page=1&per_page=50', payload=[{"id": "2"}])
         
         generator = strategy.get_async_endpoint_data()
         
